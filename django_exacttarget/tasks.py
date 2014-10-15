0d6846f91f05674c38ff4fbc792b60c785cb4098
Guillaume Pousseo <guillaumepousseo@revsquare.com>
2014-10-15 04:28:30 -1000

    re-adding tasks

---
+++ django_exacttarget/tasks.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from celery import shared_task
+
+from .service import ExactTargetService
+
+
+@shared_task
+def send_mail(recipients, email_message):
+    ExactTargetService.send_email(recipients, email_message)
